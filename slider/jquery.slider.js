2b8ef6ebd101e8f7d179dfd29ec9e2dc662d433d
Deric Mendez <mendezd@digitalev.com>
2011-11-15 17:47:34 -0600

    udpated slider script

--- slider/jquery.slider.js
+++ slider/jquery.slider.js
@@ -1,19 +1,85 @@
-(function ($) {
+(function($) {
 
 	$.easing.customSlideIn = function (x, t, b, c, d) {
 		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
 	};
-	
-    $.fn.slider = function (options) {
 
-        var settings = {
+    $.slider = function(element, options) {
+
+        var defaults = {
             'speed': 400,
             'startIndex': 0,
             'horizontal': true,
 			'easing': 'customSlideIn'
-        };
+        }
+
+        var slider = this;
+        slider.settings = $.extend({}, defaults, options);
+        
+		var $element = $(element), element = element;
+		var $elementLi = $('li', $element);
+		var childrenCount = $elementLi.size();
+		var viewportSizeNum = calculateWidthPercentage($element, $elementLi);
+		var matrix = [];
+		var closedSize = 0;
+		var activeIndex = slider.settings.startIndex;
+		var actionEvent = ('ontouchstart' in window) ? "touchstart touchend" : "mouseover";
 		
-		var createMatrix = function(n, n1, n2) {
+		$.extend(slider, {
+			init: function() {            
+				$element.css({ 
+					'overflow': 'hidden', 
+					'position' : 'relative',
+					'listStyleType': 'none',
+					'margin': 0,
+					'padding': 0
+				});			
+				$elementLi.css({ 
+					'position' : 'absolute', 
+					'overflow': 'hidden', 
+					'top': 0, 
+					'left': 0 ,
+					'listStyleType': 'none',
+					'margin': 0,
+					'padding': 0
+				});
+				
+				setSize();
+				
+				$elementLi.each(function() {
+					setPosition($(this), activeIndex);
+				}).bind(actionEvent, function(e) {
+					e.preventDefault();
+					var allowEvent = $(this).index() == activeIndex;
+					setPosition($(this));
+					return allowEvent;
+				});
+			},
+			
+			goto: function(idx) {
+				$($elementLi[idx]).trigger('mouseover');
+			}
+		});
+        
+		function setPosition(elem, idx) {
+			var siblings = elem.siblings();
+			var thisIndex = idx == undefined ? elem.index() : idx;
+			$elementLi.each(function(i) {
+				if ( slider.settings.horizontal )
+					$(this).stop().animate({ left: matrix[thisIndex][i] + '%' }, slider.settings.speed, slider.settings.easing);
+				else
+					$(this).stop().animate({ top: matrix[thisIndex][i] + '%' }, slider.settings.speed, slider.settings.easing);
+			});
+			activeIndex = thisIndex;
+		}
+		
+		function setSize() {
+			closedSize = (100 - viewportSizeNum) / (childrenCount - 1);
+			matrix = buildSizingMatrix(childrenCount, viewportSizeNum, closedSize);			
+			$element.data('viewportSizeNum', viewportSizeNum);			
+		}
+				
+		function buildSizingMatrix(n, n1, n2) {
 			var matrix = [];
 			for ( var i=0; i < n; i++ ) {
 				matrix[i] = [];
@@ -24,78 +90,37 @@
 				}
 			}
 			return matrix;
-		};
-
-        if (options)
-            $.extend(settings, options);
-
-        return this.each(function (i, el) {
-			
-			$(el).css({ 
-				'overflow': 'hidden', 
-				'position' : 'relative',
-				'listStyleType': 'none',
-				'margin': 0,
-				'padding': 0
-			});			
-			$(el).find('li').css({ 
-				'position' : 'absolute', 
-				'overflow': 'hidden', 
-				'top': 0, 
-				'left': 0 ,
-				'listStyleType': 'none',
-				'margin': 0,
-				'padding': 0
+		}
+		
+		function calculateWidthPercentage(container, lists) {
+			return Math.round((lists.width() / container.width()) * 100);
+		}
+		
+        slider.init();
+		
+		$(window).bind('resize', function () {
+			viewportSizeNum = calculateWidthPercentage($element, $elementLi);
+			setSize();
+			$elementLi.each(function() {
+				setPosition($(this), activeIndex);
 			});
-			
-            $(el).bind('slider.sizing', function () {
-                
-				var 
-					// elements
-                    $container = $(this),
-                    $lists = $container.find('li'),
-                    
-					// internal variables
-					count = $lists.size(),
-					actionEvent = ('ontouchstart' in window) ? "click" : "mouseover",
-					activeIndex = settings.startIndex;					
-				
-				// reset the width for resizing adjustments
-				$lists.css('width', '');
-				
-				var viewportSizeNum = Math.round(($lists.width() / $container.width()) * 100);					
-				var closedSize = (100 - viewportSizeNum) / (count - 1);
-				var viewportAddition = viewportSizeNum - closedSize;				
-				var matrix = createMatrix(count, viewportSizeNum, closedSize);				
-				
-				$lists.css({ 'width': (settings.horizontal ? viewportSizeNum : "100") + '%' });				
-				
-				var setPosition = function(elem, idx) {
-					var siblings = elem.siblings();
-					var thisIndex = idx == undefined ? elem.index() : idx;
-					$lists.each(function(i) {
-						if ( settings.horizontal )
-							$(this).stop().animate({ left: matrix[thisIndex][i] + '%' }, settings.speed, settings.easing);
-						else
-							$(this).stop().animate({ top: matrix[thisIndex][i] + '%' }, settings.speed, settings.easing);
-					});
-					activeIndex = thisIndex;
-				};
-				
-				$lists.each(function() {
-					setPosition($(this), activeIndex);
-				}).unbind(actionEvent).bind(actionEvent, function() {
-					var allowEvent = $(this).index() == activeIndex;
-					setPosition($(this));					
-					return allowEvent;
-				});
-				
-				
-            }).trigger('slider.sizing');
+		});
 
-            $(window).bind('resize', function () {
-                $(el).trigger('slider.sizing');
-            });
-        });
-    };
+    }
+
+    $.fn.slider = function(options) {
+		if ( typeof options == 'string') {
+			var instance = $(this).data('slider');
+			var args = Array.prototype.slice.call(arguments, 1);
+			return instance[options].apply(instance, args);
+		} else {
+			return this.each(function() {
+				if (undefined == $(this).data('slider')) {
+					var plugin = new $.slider(this, options);
+					$(this).data('slider', plugin);
+				}
+			});
+		}
+    }
+
 })(jQuery);
